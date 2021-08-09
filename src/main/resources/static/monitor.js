c6e104d06b8d665bcb913dd97ec03975ad544778
David J. Barnes <davidjonbarnes@yahoo.co.uk>
2021-08-09 09:36:49 +0100

    got show all records working, started working on search

--- src/main/resources/static/monitor.js
+++ src/main/resources/static/monitor.js
@@ -1,10 +1,26 @@
+//Return all monitors from the api
 function showAll() {
-	fetch('http://localhost:9002/monitor/findAll').then(function(response) {
-	  return response.json();
-	}).then(function(data) {
-	  document.getElementById('result').value = data;
-	}).catch(function() {
-	  document.getElementById('result').value ='No data found';
+	fetch('http://localhost:9002/monitor/findAll').then(response => {
+		if(!response.ok) {
+			throw Error("ERROR");
+		}
+		return response.json();
+	}).then(data => {
+		console.log(data);
+		const html = data.map(monitor => {
+			return `
+			<div class="monitor">
+			<p>ID: ${monitor.id} Monitor Name: ${monitor.name} Price: £${monitor.price}</p>
+			</div>
+			`;
+		})
+		.join("");
+		document.querySelector('#list').insertAdjacentHTML('afterbegin', html);
+	}).catch(error => {
+		console.log(error);
 	});
-
 }
+
+//----------------------------------------------
+//find monitor by name
+findMonitor()
